7ebe4e4ddadf05372a23566ed352f14efde2ffeb
sebasGZA <sebassga34@gmail.com>
2025-01-03 21:07:50 -0500

    feat: connect gateway con orders microservice

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,35 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Inject,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+
+import { ORDERS_SERVICE } from '../config';
+import { CreateOrderDto } from './dto/create-order.dto';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(
+    @Inject(ORDERS_SERVICE) private readonly ordersClient: ClientProxy,
+  ) {}
+
+  @Post()
+  create(@Body() createOrderDto: CreateOrderDto) {
+    return this.ordersClient.send('createOrder', createOrderDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.ordersClient.send('findAllOrders', {});
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.ordersClient.send('findOneOrder', { id });
+  }
+}
